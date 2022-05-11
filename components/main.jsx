99f0d449d3e474aef3563fdc1bd2ddb6f1986735
SorinPapa <sorin.papa@ymail.com>
2022-05-11 15:22:25 +0300

    home payment

--- components/main.jsx
+++ components/main.jsx
@@ -1,7 +1,38 @@
+import HomeNewsletter from './homepage/home-newsletter';
+
 export default function Main() {
   return (
     <main className="content">
-      <header className="content-header"></header>
+      <header className="content-header">
+        <div className="banner-main">
+          <div className="banner-main-inner container">
+            <div className="banner-info">
+              <h1>Office Suits</h1>
+              <h2>Get back to work in style</h2>
+
+              <a
+                href=""
+                title="Create your suit"
+                className="button button-large"
+              >
+                Create your suit
+              </a>
+            </div>
+
+            <div className="banner-copy">
+              <p>
+                <span>A tailored suit</span>
+                that will fit you perfectly is a dream for everybody.
+              </p>
+
+              <p className="d-none d-lg-block">
+                We provide you with the design and you choose if you like it or
+                if you want to change the details with our online configurator.
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
 
       <section className="content-main">
         <section className="home-catalog-preview container">
@@ -12,25 +43,27 @@ export default function Main() {
 
             <p>
               Accomplish new achievements with a bold new style from the
-              brilliant collection of men’s designer suits. For the finest
-              custom tailored experience, be sure to explore our selection of
-              premium fabrics from one of the world's oldest fabric mills,
-              Vitale Barberis Canonico.
+              brilliant collection of men’s designer suits.
+            </p>
+            <p>
+              For the finest custom tailored experience, be sure to explore our
+              selection of premium fabrics from one of the world's oldest fabric
+              mills, Vitale Barberis Canonico.
             </p>
           </header>
 
           <section className="product-tiles">
-            {/* insert product tiles */}
+            {/* insert product-tile */}
           </section>
 
           <footer>
-            <a href="" title="see more" className="catalog-preview-cta">
+            <a href="" title="See more" className="catalog-preview-cta">
               see more
             </a>
           </footer>
         </section>
 
-      
+        <HomeNewsletter></HomeNewsletter>
 
         <section className="home-payment container">
           <header>
@@ -41,31 +74,31 @@ export default function Main() {
 
           <ul>
             <li>
-              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+              <img src="/images/payment/amazon.jpg" alt=""></img>
             </li>
 
             <li>
-              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+              <img src="/images/payment/cirrus.jpg" alt=""></img>
             </li>
 
             <li>
-              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+              <img src="/images/payment/amazon.jpg" alt=""></img>
             </li>
 
             <li>
-              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+              <img src="/images/payment/amazon.jpg" alt=""></img>
             </li>
 
             <li>
-              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+              <img src="/images/payment/amazon.jpg" alt=""></img>
             </li>
 
             <li>
-              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+              <img src="/images/payment/amazon.jpg" alt=""></img>
             </li>
 
             <li>
-              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+              <img src="/images/payment/amazon.jpg" alt=""></img>
             </li>
           </ul>
         </section>
