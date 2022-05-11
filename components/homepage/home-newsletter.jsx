59108524c2dd00675b52964d4716cea67cd63e4c
SorinPapa <sorin.papa@ymail.com>
2022-05-11 15:06:08 +0300

    home payments

---
+++ components/homepage/home-newsletter.jsx
@@ -0,0 +1,7 @@
+export default function HomeNewsLetter() {
+  return (
+    <section className="home-newsletter">
+      <form action="" className="container"></form>
+    </section>
+  );
+}
--- components/main.jsx
+++ components/main.jsx
@@ -1,3 +1,77 @@
 export default function Main() {
-  return <main>m</main>;
+  return (
+    <main className="content">
+      <header className="content-header"></header>
+
+      <section className="content-main">
+        <section className="home-catalog-preview container">
+          <header>
+            <h1 className="section-title">
+              <span>Glamour</span> Suits
+            </h1>
+
+            <p>
+              Accomplish new achievements with a bold new style from the
+              brilliant collection of men’s designer suits. For the finest
+              custom tailored experience, be sure to explore our selection of
+              premium fabrics from one of the world's oldest fabric mills,
+              Vitale Barberis Canonico.
+            </p>
+          </header>
+
+          <section className="product-tiles">
+            {/* insert product tiles */}
+          </section>
+
+          <footer>
+            <a href="" title="see more" className="catalog-preview-cta">
+              see more
+            </a>
+          </footer>
+        </section>
+
+        <section className="home-newsletter">
+          x
+        </section>
+
+        <section className="home-payment container">
+          <header>
+            <h1 className="section-title">
+              <span>How to</span> Pay
+            </h1>
+          </header>
+
+          <ul>
+            <li>
+              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+            </li>
+
+            <li>
+              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+            </li>
+
+            <li>
+              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+            </li>
+
+            <li>
+              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+            </li>
+
+            <li>
+              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+            </li>
+
+            <li>
+              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+            </li>
+
+            <li>
+              <img src="/images/payment/amazon.jpg" alt="amazaon" />
+            </li>
+          </ul>
+        </section>
+      </section>
+    </main>
+  );
 }
